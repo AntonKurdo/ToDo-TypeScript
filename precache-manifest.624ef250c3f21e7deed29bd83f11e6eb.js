880d8fa60698f44071a64dba8d9a55b4f33e462e
Anton Kurdo <anton.kurdo@gmail.com>
2020-07-17 18:03:55 +0300

    Updates

---
+++ precache-manifest.624ef250c3f21e7deed29bd83f11e6eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91bd2b985c5238a09f0156b07b2b5e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89857328b79390f8ac85",
+    "url": "/static/css/main.0b39c23d.chunk.css"
+  },
+  {
+    "revision": "31a2f43ea26959b67505",
+    "url": "/static/js/2.ecf26313.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ecf26313.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89857328b79390f8ac85",
+    "url": "/static/js/main.1ed63f63.chunk.js"
+  },
+  {
+    "revision": "d049e954b1c74f29178c",
+    "url": "/static/js/runtime-main.9c7c50c1.js"
+  }
+]);
